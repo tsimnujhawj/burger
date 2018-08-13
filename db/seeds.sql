2ac847bb8845d7354e4e1d54fa65f6ae852e8ea8
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-13 15:47:48 -0500

    added db data

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (
+    burger_name
+) VALUE (
+    "Cajun Burger"
+);
+
+INSERT INTO burgers (
+    burger_name
+) VALUE (
+    "Jucy Lucy"
+);
+
+INSERT INTO burgers (
+    burger_name
+) VALUE (
+    "California Burger"
+);
