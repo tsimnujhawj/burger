b6298b42baf72ee2e62d12b27d5dc24465c3bc62
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-15 17:44:29 -0500

    config

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const hbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller");
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.engine("handlebars", hbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, ()=> {
+  console.log("App now listening at localhost:" + PORT);
+});
