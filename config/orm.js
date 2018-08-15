0584f13ea6f5823b4f753225496404be7bb6ac8e
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-15 12:49:45 -0500

    fix

--- config/orm.js
+++ config/orm.js
@@ -1,39 +1,39 @@
 let connection = require('./connection.js');
 
 let orm = {
-  selectAll: (table, callback)=>{
+  selectAll: (table, cb)=>{
     let queryString = 'SELECT * FROM ' + table;
 
     connection.query(queryString, (error, data)=>{
       if(error) throw error;
-      callback(data);
+      cb(data);
     });
   },
 
-  insertOne: (table, column, burgerInput, callback)=>{
+  insertOne: (table, column, burgerInput, cb)=>{
     let queryString = 'INSERT INTO ' + table + '(' + column + ') VALUES (?)';
 
     connection.query(queryString, [burgerInput], (error, data)=>{
       if(error) throw error;
-      callback(data);
+      cb(data);
     });
   },
 
-  updateOne: (table, col, colVal, condition, conditionVal, callback)=>{
+  updateOne: (table, col, colVal, condition, conditionVal, cb)=>{
     let queryString = 'UPDATE ' + table + ' SET ' + col + '=?' + 'WHERE ' + condition + '=?';
 
     connection.query(queryString, [colVal, conditionVal], (error, data)=>{
       if(error) throw error;
-      callback(data);
+      cb(data);
     });
   },
 
-  deleteOne: (table, condition, conditionVal, callback)=>{
+  deleteOne: (table, condition, conditionVal, cb)=>{
     let queryString = 'DELETE FROM ' + table + ' WHERE ' + condition + '=?';
 
     connection.query(queryString, [conditionVal], (error, data)=>{
       if(error) throw error;
-      callback(data);
+      cb(data);
     });
   }
 };
