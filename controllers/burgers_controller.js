6af914d86376172f09b1bb7ad382c11ebb8d0f6a
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-16 13:30:32 -0500

    fix

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,25 +3,28 @@ const router = express.Router();
 const burger = require("../models/burger")
 
 router.get("/", (req, res)=>{
-    let information = {
-        burg: [],
-    };
-    // get data from burger table
     burger.selectAll((data)=>{
-        for (let i = 0; i < data.length; i++){
-            information.burg.push(data[i]);
+        let information = {
+            burg: data
         };
         res.render("index", information)
     })
+    // // get data from burger table
+    // burger.selectAll((data)=>{
+    //     for (let i = 0; i < data.length; i++){
+    //         information.burg.push(data[i]);
+    //     };
+    //     res.render("index", information)
+    // })
 })
 
 router.post("/burger", (req, res)=>{
     burger.insertOne(["burger_name"], [req.body.burger_name], (data)=>{
-    console.log(data);
+    res.redirect("/");
 })
 })
 
-router.put("/burgers/:id", (req, res)=>{
+router.put("/burger/:id", (req, res)=>{
     let condition = "id = " + req.params.id;
     burger.updateOne({
         devoured: true
--- models/burger.js
+++ models/burger.js
@@ -4,21 +4,21 @@ const orm = require("../config/orm.js");
 let burger = {
     // Select all burger table entries
     selectAll: (cb)=> {
-      orm.selectAll('burgers', function(res) {
+      orm.selectAll('burgers', (res)=> {
         cb(res);
       });
     },
   
     // The letiables cols and vals are arrays
     insertOne: (cols, vals, cb)=> {
-      orm.insertOne('burgers', cols, vals, function(res) {
+      orm.insertOne('burgers', cols, vals, (res)=> {
         cb(res);
       });
     },
   
     // The objColVals is an object specifying columns as object keys with associated values
     updateOne: (objColVals, condition, cb)=> {
-      orm.updateOne('burgers', objColVals, condition, function(res) {
+      orm.updateOne('burgers', objColVals, condition, (res)=> {
         cb(res);
       });
     }
