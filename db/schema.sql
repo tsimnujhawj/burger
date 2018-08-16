029d812f00d124edd367c70d0bacb12784a25805
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-16 15:46:53 -0500

    fix

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE burgers (
     id INT(10) NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
